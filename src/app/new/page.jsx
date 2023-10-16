048e29dbe3dcc3947e5419e626d292927b286158
BricoPhonix <brico@phonix.network>
2023-10-15 23:29:36 -0500

    03:26:22 Formulario en React hasta 03:36:55

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,4 +1,54 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 function NewPage() {
-  return <div>Formulario de tareas</div>;
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    router.push("/");
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10 lg:w-1/4 md:w-1/2" onSubmit={onSubmit}>
+        <label htmlFor="title" className="font-bold text-sm">
+          Título de la tarea
+        </label>
+        <input
+          id="title"
+          type="text"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Título"
+        />
+
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
+        <textarea
+          id="description"
+          rows="3"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          placeholder="Describe tu tarea"
+        ></textarea>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Crear
+        </button>
+      </form>
+    </div>
+  );
 }
 export default NewPage;
