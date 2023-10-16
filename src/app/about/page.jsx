d0b6597cc9d0227d5060fd66f6e77bd0fcdaabc9
BricoPhonix <brico@phonix.network>
2023-10-16 11:17:56 -0500

    04:05:10 Navegacion hasta 04:12:24

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,22 @@
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <section className="container mx-auto">
+      <h3 className="text-4xl font-bold my-5">About</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+        expedita ea, saepe ipsam voluptatum, eos vel quo architecto sunt alias
+        id! Atque nobis saepe delectus laborum aliquid dolore ex dolor totam
+        odio tempore facere earum rem est perferendis illo eius temporibus
+        necessitatibus, possimus assumenda fuga nam eaque porro nostrum? Impedit
+        nemo ab minima, vero earum itaque non. Sed explicabo temporibus
+        voluptatibus quis perferendis voluptatum tempore repellendus, possimus
+        quae amet at, veniam velit obcaecati! Iste commodi quo repellendus
+        cupiditate, obcaecati, sit voluptatem corporis exercitationem dolor,
+        tempora recusandae incidunt eligendi nemo impedit provident omnis
+        consectetur? Incidunt, dolores magnam reprehenderit ab voluptate
+        delectus!
+      </p>
+    </section>
+  );
 }
 export default AboutPage;
